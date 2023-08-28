7e7d7538cdaef5e46c7a2237f8ce31b62ac0a08d
HamzaDogann <hamzadgn011@gmail.com>
2023-08-28 22:10:26 +0300

    HTML  CSS JS ile Slider Tasarımı
    HTML  CSS JS ile Slider Tasarımı

---
+++ Slider/script.js
@@ -0,0 +1,22 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    grabCursor:'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      
+    },
+
+    breakpoints :{
+        0:{
+            slidesPerView :1,
+        },
+        520:{
+            slidesPerView :2,
+        },
+        950:{
+            slidesPerView :3,
+        }
+    },
+  });
